bcc7aa7f30b47486fb1c993f4dcace3d30c856c9
alexander <alexander@testparty.ai>
2025-07-24 10:55:15 -0400

    amk - region route

---
+++ src/app/pages/region/import/import.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import',
+  standalone: true,
+  templateUrl: './import.component.html'
+})
+export class ImportComponent {}
